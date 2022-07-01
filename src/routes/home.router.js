e5198865959f05459f776703741e4808bf40b715
NamThaPhu <thaiphuongnam1071@gmail.com>
2022-07-01 23:41:12 +0700

    .

--- src/routes/home.router.js
+++ src/routes/home.router.js
@@ -2,7 +2,22 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
-    res.send('API')
+    const info = {
+        author: 'Thái Phương Nam',
+        nickName: 'NamThaPhu',
+        apiName: 'api-nghiquan',
+        documents: [
+            {
+                name: product,
+                path: 'https://api-nghiquan.herokuapp.com/products',
+            },
+            {
+                name: category,
+                path: 'https://api-nghiquan.herokuapp.com/categories',
+            },
+        ]
+    }
+    res.json(info)
 })
 
 module.exports = router
